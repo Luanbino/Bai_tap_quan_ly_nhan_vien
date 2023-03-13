36c39418dd45469b845bb3e1bc228cbed94a86bd
Luanbino <Luanbino120598@gmail.com>
2023-03-13 20:24:48 +0700

    xong

---
+++ models/NhanVien.js
@@ -0,0 +1,15 @@
+
+function NhanVien() {
+    this.taiKhoanNV = '';
+    this.tenNhanVien = '';
+    this.email = '';
+    this.matKhau = '';
+    this.ngayLam = '';
+    this.luongCB = '';
+    this.chucVu = '';
+    this.heSoLuong = '';
+    this.gioLam = '';
+    this.tongLuong = '';
+    this.xepLoai = '';
+}
+
